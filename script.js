3633d30883961d9df7f93adaf896a4d2c37c4c4a
raisulhamim <raisulhamim.dev@gmail.com>
2025-09-22 17:48:10 +0600

    condition console.log command done

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
 let age = 24
 let phone = 150000
-let dailysomoy=8
-let giftcost=35000
-let perdaycall=100
-let shompod =7000000
+let dailysomoy = 8
+let giftcost = 35000
+let perdaycall = 100
+let shompod = 7000000
 
 
 if (age > 23) {
-    if(phone>100000) {
-        if(dailysomoy> 4){
-            if(giftcost>20000){
-                 if(perdaycall>20){
-                    if(shompod>5000000){
+    if (phone > 100000) {
+        if (dailysomoy > 4) {
+            if (giftcost > 20000) {
+                if (perdaycall > 20) {
+                    if (shompod > 5000000) {
                         console.log("I LOVE YOU TOO,koi chila tmi ato din ,ami to tomakei chai.")
 
 
-                    }else{
+                    } else {
                         console.log("gorib hoiya aise amar moto may er shatay prem korte,ja shor,RIJECT");
 
                     }
+                     console.log("sotti tmi amaky ato gula call diba ,ohhh bisoy ta cute to")
 
-                 }else{
-                    console.log("amar khobor barbar nite na parle prem korbe kmne");
 
-                 }
+                } else {
+                    console.log("amar khoj khobor barbar nite na parle prem korbe kmne");
 
-            }else{
-               console.log("gift dite parbe na taile prem korte chas kn");
+                }
+                 console.log("wow,ato tk gift amar janno,nice")
+
+            } else {
+                console.log("gift dite parbe na taile prem korte chas kn");
             }
+             console.log("tmi amaky ato somoy diba ,bisoy ta cute to")
 
-        }else{
-          console.log("shomoy dite parbe na abar prem ar nam muk a nes kmne");
+        } else {
+            console.log("shomoy dite parbe na abar prem ar nam muk a nes kmne");
 
         }
+         console.log("ohh tmi iphone chalao nice.")
 
-    }else{
+    } else {
         console.log("nokia phone niya aisay prem korte");
     }
+    console.log("tmr boyosh thik asay")
 } else {
     console.log("agay boro hou tarpor prem koiro");
 
