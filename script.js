7c2ee542927e65178b7ce860e20261805a79c8a3
raisulhamim <raisulhamim.dev@gmail.com>
2025-09-29 16:51:23 +0600

    I am use for loop and condition to skip the reject value

--- script.js
+++ script.js
@@ -1,85 +1,97 @@
-let age = 24
-let phone = 150000
-let dailysomoy = 8
-let giftcost = 35000
-let perdaycall = 100
-let shompod = 7000000
+// let age = 24
+// let phone = 150000
+// let dailysomoy = 8
+// let giftcost = 35000
+// let perdaycall = 100
+// let shompod = 7000000
 
 
-if (age > 23) {
-    if (phone > 100000) {
-        if (dailysomoy > 4) {
-            if (giftcost > 20000) {
-                if (perdaycall > 20) {
-                    if (shompod > 5000000) {
-                        console.log("I LOVE YOU TOO,koi chila tmi ato din ,ami to tomakei chai.")
+// if (age > 23) {
+//     if (phone > 100000) {
+//         if (dailysomoy > 4) {
+//             if (giftcost > 20000) {
+//                 if (perdaycall > 20) {
+//                     if (shompod > 5000000) {
+//                         console.log("I LOVE YOU TOO,koi chila tmi ato din ,ami to tomakei chai.")
 
 
-                    } else {
-                        console.log("gorib hoiya aise amar moto may er shatay prem korte,ja shor,RIJECT");
+//                     } else {
+//                         console.log("gorib hoiya aise amar moto may er shatay prem korte,ja shor,RIJECT");
 
-                    }
-                    console.log("sotti tmi amaky ato gula call diba ,ohhh bisoy ta cute to")
+//                     }
+//                     console.log("sotti tmi amaky ato gula call diba ,ohhh bisoy ta cute to")
 
 
-                } else {
-                    console.log("amar khoj khobor barbar nite na parle prem korbe kmne");
+//                 } else {
+//                     console.log("amar khoj khobor barbar nite na parle prem korbe kmne");
 
-                }
-                console.log("wow,ato tk gift amar janno,nice")
+//                 }
+//                 console.log("wow,ato tk gift amar janno,nice")
 
-            } else {
-                console.log("gift dite parbe na taile prem korte chas kn");
-            }
-            console.log("tmi amaky ato somoy diba ,bisoy ta cute to")
+//             } else {
+//                 console.log("gift dite parbe na taile prem korte chas kn");
+//             }
+//             console.log("tmi amaky ato somoy diba ,bisoy ta cute to")
 
-        } else {
-            console.log("shomoy dite parbe na abar prem ar nam muk a nes kmne");
+//         } else {
+//             console.log("shomoy dite parbe na abar prem ar nam muk a nes kmne");
 
-        }
-        console.log("ohh tmi iphone chalao nice.")
+//         }
+//         console.log("ohh tmi iphone chalao nice.")
 
-    } else {
-        console.log("nokia phone niya aisay prem korte");
-    }
-    console.log("tmr boyosh thik asay")
-} else {
-    console.log("agay boro hou tarpor prem koiro");
+//     } else {
+//         console.log("nokia phone niya aisay prem korte");
+//     }
+//     console.log("tmr boyosh thik asay")
+// } else {
+//     console.log("agay boro hou tarpor prem koiro");
 
-}
+// }
 
 
 // ========LOOP==========
 
-for (let h = 1; h < 100; h++) {
-    console.log("i love you")
+// for (let h = 1; h < 100; h++) {
+//     console.log("i love you")
 
 
-}
+// }
 
 // ======= NAMTA ==========
-let namta = 3
-let i = 1
-while (i <= 10) {
-    console.log(namta + "x" + i + "=" + namta * i);
-    i++
-}
+// let namta = 3
+// let i = 1
+// while (i <= 10) {
+//     console.log(namta + "x" + i + "=" + namta * i);
+//     i++
+// }
 
 // =======USER INPUT LOOP===========
 
-let h = Number(prompt("Enter a first value"))
-let k = Number(prompt("Enter a last value"))
+let firstvalue = Number(prompt("Enter a first value"));
+let lastvalue = Number(prompt("Enter a last value"));
+let reject1 = Number(prompt("Enter a first reject value")); 
+let reject2 = Number(prompt("Enter a second reject value")); 
 
-for (let f = h; f <= k; f++) {
-    console.log("~~~~~~~~~~~~~");
-    for (let i = 1; i <= 10; i++) {
-        console.log(f + "x" + i + "=" + f * i);
+for (let h = firstvalue; h <= lastvalue; h++) {
+  console.log( h,"এর নামতা start ====");
+
+  for (let i = 1; i <= 10; i++) {
+    let result = h * i;
+
+    
+    if (result === reject1 ||result === reject2 ) {
+      continue;
+    }
+
+    console.log(h + " x " + i + " = " +result);}
+    if(h<lastvalue){
+        console.log(h,"এর নামতা end ====")
+    }else{
+          console.log("সকল নামতার সমাপ্তি ঘোষণা করা হলো")
     }
 }
 
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
-}
+
 
 // =========ARRAY=========
 
@@ -90,51 +102,51 @@ for (let i = 0; i <= 10; i++) {
 
 // ==========OBJECT=========
 
-let obj = {
-    name: "raisul",
-    age: 21,
-    email: "raisulhamim.dev@gmail.com",
-    number: 38749858
+// let obj = {
+//     name: "raisul",
+//     age: 21,
+//     email: "raisulhamim.dev@gmail.com",
+//     number: 38749858
 
-}
-console.log(obj.email);
+// }
+// console.log(obj.email);
 
 
 
 // ========ARRAY OBJECT=====
 
-let object= [{
-    name: "raisul",
-    age: 21,
-    email: "raisulhamim.dev@gmail.com",
-    number: 38749858
-},
-   {
-    name: "sofiq",
-    age: 22,
-    email: "sofiuq112@gmail.com",
-    number: 38749255
-},
- {
-    name: "rakib",
-    age: 25,
-    email: "hasanrakib339@gmail.com",
-    number: 387583553
-}
-]
+// let object= [{
+//     name: "raisul",
+//     age: 21,
+//     email: "raisulhamim.dev@gmail.com",
+//     number: 38749858
+// },
+//    {
+//     name: "sofiq",
+//     age: 22,
+//     email: "sofiuq112@gmail.com",
+//     number: 38749255
+// },
+//  {
+//     name: "rakib",
+//     age: 25,
+//     email: "hasanrakib339@gmail.com",
+//     number: 387583553
+// }
+// ]
   
     
-console.log(object[2].name+ object[2].age);
+// console.log(object[2].name+ object[2].age);
   
 
 
  
 
-let arr=["akib", "rakib", "sakib","sofik", "joy", "roy", "alif","raisul"]
+// let arr=["akib", "rakib", "sakib","sofik", "joy", "roy", "alif","raisul"]
 
 
-for(let i=0; i<arr.length; i++){
-    console.log(arr[i]);
-}
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i],i);
+// }
 
 
