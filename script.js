1a38ed27c7f7417b9c549c894ea5697be3e6c74a
raisulhamim <raisulhamim.dev@gmail.com>
2025-10-10 19:32:06 +0600

    marksit some change fixed javascript

--- script.js
+++ script.js
@@ -216,16 +216,16 @@ if (mark <= 100 && mark >= 80) {
     console.log("A");
 
 } else if (mark < 70 && mark >= 60) {
-    console.log("A");
+    console.log("A-");
 
 } else if (mark < 60 && mark >= 50) {
-    console.log("A");
+    console.log("B");
 
 } else if (mark < 50 && mark >= 40) {
-    console.log("A");
+    console.log("C");
 
 } else if (mark < 40 && mark >= 33) {
-    console.log("A");
+    console.log("D");
 
 } else if (mark > 100) {
     console.log("Enter a valid number");
