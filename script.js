88e5d6e69f8c83026156a59f663536a2f2c9e296
raisulhamim <raisulhamim.dev@gmail.com>
2025-10-11 20:38:41 +0600

    javaScript if else condition use and solve full problem

--- script.js
+++ script.js
@@ -212,19 +212,19 @@ if (mark <= 100 && mark >= 80) {
     console.log("A+");
 
 
-} else if (mark < 80 && mark >= 70) {
+} else if (mark <=79 && mark >= 70) {
     console.log("A");
 
-} else if (mark < 70 && mark >= 60) {
+} else if (mark <=69 && mark >= 60) {
     console.log("A-");
 
-} else if (mark < 60 && mark >= 50) {
+} else if (mark <=59 && mark >= 50) {
     console.log("B");
 
-} else if (mark < 50 && mark >= 40) {
+} else if (mark <=49 && mark >= 40) {
     console.log("C");
 
-} else if (mark < 40 && mark >= 33) {
+} else if (mark <=39 && mark >= 33) {
     console.log("D");
 
 } else if (mark > 100) {
